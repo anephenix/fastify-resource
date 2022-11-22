78f39bdfea80c56a424026e0d1241f50cdebeedd
Paul Jensen <paulbjensen@gmail.com>
2022-11-22 10:16:03 +0000

    Linting

--- src/controller.ts
+++ src/controller.ts
@@ -24,7 +24,7 @@ type ActionServiceMapping = {
   get: ServiceKey;
   update: ServiceKey;
   delete: ServiceKey;
-}
+};
 type ActionServiceMappingKey = keyof typeof actionServiceMapping;
 type ServiceKey = 'getAll' | 'create' | 'get' | 'update' | 'delete';
 
@@ -34,7 +34,7 @@ type Controller = {
   get?: any;
   update?: any;
   delete?: any;
-}
+};
 
 const getCodeForError = (error: Error): StatusCode => {
   switch (error.message) {
@@ -61,45 +61,53 @@ const handleResponse = ({
   }
 };
 
-const actionServiceMapping:ActionServiceMapping = {
+const actionServiceMapping: ActionServiceMapping = {
   index: 'getAll',
   create: 'create',
   get: 'get',
   update: 'update',
   delete: 'delete',
-}
+};
 
-const getParams = (action:ActionServiceMappingKey, req: Request) => { 
+const getParams = (action: ActionServiceMappingKey, req: Request) => {
   const actionsWithBody = ['create', 'update'];
   if (actionsWithBody.includes(action)) {
     return Object.assign({}, req.params, req.body);
   }
   return req.params;
-}
+};
 
-const getResponseData = (action:ActionServiceMappingKey, { success, data, error, rep } : HandleResponseParams):HandleResponseParams => {
+const getResponseData = (
+  action: ActionServiceMappingKey,
+  { success, data, error, rep }: HandleResponseParams
+): HandleResponseParams => {
   if (action === 'create') {
     return { success, data, error, rep, successCode: 201 };
   }
   return { success, data, error, rep };
-}
+};
 
-const generateAction = (action:ActionServiceMappingKey, service: Service) => {
+const generateAction = (action: ActionServiceMappingKey, service: Service) => {
   return async (req: Request, rep: Reply) => {
     const params = getParams(action, req);
-    const method:ServiceKey = actionServiceMapping[action];
+    const method: ServiceKey = actionServiceMapping[action];
     const { success, data, error } = await service[method](params);
     const responseData = getResponseData(action, { success, data, error, rep });
     return handleResponse(responseData);
-  }
-}
+  };
+};
 
 const controllerGenerator = (service: Service) => {
-  const controller:Controller = {};
-  const actions = Object.keys(actionServiceMapping) as ActionServiceMappingKey[];
+  const controller: Controller = {};
+  const actions = Object.keys(
+    actionServiceMapping
+  ) as ActionServiceMappingKey[];
   actions.forEach(action => {
-    controller[action] = generateAction(action as ActionServiceMappingKey, service);
-  })
+    controller[action] = generateAction(
+      action as ActionServiceMappingKey,
+      service
+    );
+  });
   return controller;
 };
 

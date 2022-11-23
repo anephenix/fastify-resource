a19aea200bd4131e8a28ccf30f590c169db449eb
Paul Jensen <paulbjensen@gmail.com>
2022-11-23 14:45:27 +0000

    Refactoring

--- src/service.ts
+++ src/service.ts
@@ -7,52 +7,41 @@ const objectWithoutKey = (object: any, key: string) => {
   return otherKeys;
 };
 
+const modelAction = async (action: string, model: ModelType, params: Params) => {
+  switch (action) {
+    case 'getAll':
+      return await model.query().where(params);
+    case 'get':
+      return await model.query().where(params);//.first();
+    case 'create':
+      return await model.query().insert(params);
+    case 'update':
+      return await model.query().patchAndFetchById(params.id, objectWithoutKey(params, 'id'));
+    case 'delete':
+      await model.query().deleteById(params.id);
+      return params.id;
+  }
+}
+
+const serviceFunction = (action: string, model: ModelType) => {
+  return async (params: Params) => {
+    try {
+      const data = await modelAction(action, model, params);
+      return { success: true, data };
+    } catch (error) {
+      return { success: false, error };
+    }
+  }
+}
+
 // The generator function
 function serviceGenerator(model: ModelType) {
   return {
-    getAll: async (params: Params) => {
-      try {
-        const data = await model.query().where(params);
-        return { success: true, data };
-      } catch (error) {
-        return { success: false, error };
-      }
-    },
-    create: async (params: Params) => {
-      try {
-        const data = await model.query().insert(params);
-        return { success: true, data };
-      } catch (error) {
-        return { success: false, error };
-      }
-    },
-    get: async (params: Params) => {
-      try {
-        const data = await model.query().where(params);
-        return { success: true, data };
-      } catch (error) {
-        return { success: false, error };
-      }
-    },
-    update: async (params: Params) => {
-      try {
-        const updateParams = objectWithoutKey(params, 'id');
-        const data = await model
-          .query()
-          .patchAndFetchById(params.id, updateParams);
-        return { success: true, data };
-      } catch (error) {
-        return { success: false, error };
-      }
-    },
-    delete: async (params: Params) => {
-      try {
-        await model.query().deleteById(params.id);
-        return { success: true, data: params.id };
-      } catch (error) {
-        return { success: false, error };
-      }
-    },
+    getAll: serviceFunction('getAll', model),
+    create: serviceFunction('create', model),
+    get: serviceFunction('get', model),
+    update: serviceFunction('update', model),
+    delete: serviceFunction('delete', model),
   };
 }
 
